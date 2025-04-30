d1ca46f82ec8a5f20afa0953376188905cab47c9
Rabi <rabinarayanpatra1999@gmail.com>
2025-04-30 12:32:14 +0530

    Initial commit

---
+++ src/main/java/io/github/rabinarayanpatra/sanitizer/App.java
@@ -0,0 +1,10 @@
+package io.github.rabinarayanpatra.sanitizer;
+
+/**
+ * Hello world!
+ */
+public class App {
+  public static void main( String[] args ) {
+    System.out.println( "Hello World!" );
+  }
+}

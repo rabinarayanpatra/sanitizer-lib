485ec60b0e13e155f23f4e4bcdd2507faf8a93e9
Rabinarayan Patra <rabinarayanpatra1999@gmail.com>
2025-05-01 14:56:19 +0530

    feat: add component scanning to SanitizerAutoConfiguration

--- sanitizer-spring/src/main/java/io/github/rabinarayanpatra/sanitizer/spring/config/SanitizerAutoConfiguration.java
+++ sanitizer-spring/src/main/java/io/github/rabinarayanpatra/sanitizer/spring/config/SanitizerAutoConfiguration.java
@@ -1,6 +1,7 @@
 package io.github.rabinarayanpatra.sanitizer.spring.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.Module;
@@ -16,6 +17,7 @@ import io.github.rabinarayanpatra.sanitizer.spring.jackson.SanitizerModule;
  * @since 1.0.0
  */
 @Configuration
+@ComponentScan( "io.github.rabinarayanpatra.sanitizer.spring" )
 public class SanitizerAutoConfiguration {
 
   /**

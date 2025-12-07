69e7e8eca1e82cee58f9c6232cf4a429f0f7cc5c
Rabi <rabinarayanpatra1999@gmail.com>
2025-12-07 18:38:56 +0530

    chore: migrate to vanniktech maven publish plugin for central portal

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     `maven-publish`
     id("io.spring.dependency-management") version "1.1.7"
     id("com.diffplug.spotless") version "6.25.0"
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 allprojects {
@@ -17,15 +18,12 @@ allprojects {
 subprojects {
     apply(plugin = "java-library")
     apply(plugin = "io.spring.dependency-management")
-    apply(plugin = "maven-publish")
-    apply(plugin = "signing")
+    apply(plugin = "com.vanniktech.maven.publish")
 
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(21))
         }
-        withSourcesJar()
-        withJavadocJar()
     }
 
     dependencyManagement {
@@ -41,59 +39,36 @@ subprojects {
     dependencies {
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     }
-    
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                from(components["java"])
-                
-                pom {
-                    name.set(project.name)
-                    description.set("Sanitizer Library")
-                    url.set("https://github.com/rabinarayanpatra/sanitizer-lib")
-                    licenses {
-                        license {
-                            name.set("MIT License")
-                            url.set("https://opensource.org/licenses/MIT")
-                        }
-                    }
-                    developers {
-                        developer {
-                            id.set("rabinarayanpatra")
-                            name.set("Rabinarayan Patra")
-                            email.set("rabinarayanpatra1999@gmail.com")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:git://github.com/rabinarayanpatra/sanitizer-lib.git")
-                        developerConnection.set("scm:git:ssh://github.com/rabinarayanpatra/sanitizer-lib.git")
-                        url.set("https://github.com/rabinarayanpatra/sanitizer-lib")
-                    }
+
+    mavenPublishing {
+        publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.CENTRAL_PORTAL)
+        signAllPublications()
+        
+        coordinates(group.toString(), project.name, version.toString())
+
+        pom {
+            name.set(project.name)
+            description.set("Sanitizer Library")
+            url.set("https://github.com/rabinarayanpatra/sanitizer-lib")
+            licenses {
+                license {
+                    name.set("MIT License")
+                    url.set("https://opensource.org/licenses/MIT")
                 }
             }
-        }
-        repositories {
-            maven {
-                name = "OSSRH"
-                url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-                credentials {
-                    username = System.getenv("OSSRH_USERNAME") ?: project.findProperty("ossrhUsername") as String?
-                    password = System.getenv("OSSRH_PASSWORD") ?: project.findProperty("ossrhPassword") as String?
+            developers {
+                developer {
+                    id.set("rabinarayanpatra")
+                    name.set("Rabinarayan Patra")
+                    email.set("rabinarayanpatra1999@gmail.com")
                 }
             }
+            scm {
+                connection.set("scm:git:git://github.com/rabinarayanpatra/sanitizer-lib.git")
+                developerConnection.set("scm:git:ssh://github.com/rabinarayanpatra/sanitizer-lib.git")
+                url.set("https://github.com/rabinarayanpatra/sanitizer-lib")
+            }
         }
-    }
-
-    configure<SigningExtension> {
-        val signingKey = System.getenv("SIGNING_KEY") ?: project.findProperty("signingKey") as String?
-        val signingPassword = System.getenv("SIGNING_PASSWORD") ?: project.findProperty("signingPassword") as String?
-
-        if (signingKey != null && signingPassword != null) {
-            useInMemoryPgpKeys(signingKey, signingPassword)
-        } else {
-            useGpgCmd()
-        }
-        sign(publishing.publications["maven"])
     }
 
     apply(plugin = "com.diffplug.spotless")
